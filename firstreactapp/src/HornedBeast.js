98623f4affd5d2065085cb4af0b311d09e11d539
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-20 14:53:12 -0500

    Update HornedBeast.js

--- firstreactapp/src/HornedBeast.js
+++ firstreactapp/src/HornedBeast.js
@@ -1,12 +1,20 @@
+import {useState} from "react";
 
 //this is my hornbeast component that has props as an argument this component inherits the title, desription, and image from the main component
 
 function HornedBeast(props) {
+    const [favorites, setFavorites] = useState(0);
+
+    function state(){
+        setFavorites(favorites+1)
+    }
     return (
-        <div>
+        <div onclick={state}>
+
             <h2> {props.title} </h2> 
              <p>{props.description}</p>
-             <img src={props.image}/>
+             <img src={props.image} alt={""}/>
+             <p>&#9829;{favorites}</p>
         </div>
         
 
