b3f3d0d32b868863301f5d5ca6585dc946b94c87
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-20 10:36:38 -0500

    Feature 1 complete

--- firstreactapp/src/Main.js
+++ firstreactapp/src/Main.js
@@ -1,14 +1,32 @@
 import HornedBeast from './HornedBeast';
+import data from './data.json';
+
 
 //this is the main component that has 2 hornbeast components with 2 properties each
 function Main() {
     return (
         <div>
             <main> Main </main>
-            <HornedBeast title="Wooly-Mammonth" description="This wooly mammonth is named Peaches well known from the movie Ice Age" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnckTYm2kmp22DaOiX-KE6rVk0vzgaMgnMn3iu_jqj5g&usqp=CAU&ec=48600112" />
-
-            <HornedBeast title="Elephant" description="This elephant cartoon is from the movie Dumbo" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2zlDkToJKOJiJGhocDM5dZ7nMG57RS6KETxwxj3bCIg&usqp=CAU&ec=48600112" />
-
+            <HornedBeast title={data[0].title} description={data[0].description} image={data[0].image_url} />
+            <HornedBeast title={data[1].title} description={data[1].description} image={data[1].image_url} />
+            <HornedBeast title={data[2].title} description={data[2].description} image={data[2].image_url} />
+            <HornedBeast title={data[3].title} description={data[3].description} image={data[3].image_url} />
+            <HornedBeast title={data[4].title} description={data[4].description} image={data[4].image_url} />
+            <HornedBeast title={data[5].title} description={data[5].description} image={data[5].image_url} />
+            <HornedBeast title={data[6].title} description={data[6].description} image={data[6].image_url} />
+            <HornedBeast title={data[7].title} description={data[7].description} image={data[7].image_url} />
+            <HornedBeast title={data[8].title} description={data[8].description} image={data[8].image_url} />
+            <HornedBeast title={data[9].title} description={data[9].description} image={data[9].image_url} />
+            <HornedBeast title={data[10].title} description={data[10].description} image={data[10].image_url} />
+            <HornedBeast title={data[11].title} description={data[11].description} image={data[11].image_url} />
+            <HornedBeast title={data[12].title} description={data[12].description} image={data[12].image_url} />
+            <HornedBeast title={data[13].title} description={data[13].description} image={data[13].image_url} />
+            <HornedBeast title={data[14].title} description={data[14].description} image={data[14].image_url} />
+            <HornedBeast title={data[15].title} description={data[15].description} image={data[15].image_url} />
+            <HornedBeast title={data[16].title} description={data[16].description} image={data[16].image_url} />
+            <HornedBeast title={data[17].title} description={data[17].description} image={data[17].image_url} />
+            <HornedBeast title={data[18].title} description={data[18].description} image={data[18].image_url} />
+            <HornedBeast title={data[19].title} description={data[19].description} image={data[19].image_url} />
         </div>
     );
 }
