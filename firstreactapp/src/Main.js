28a3f0f9564d62e8d0c2bc7c2215d34f28556b64
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-24 13:50:49 -0500

    sdf

--- firstreactapp/src/Main.js
+++ firstreactapp/src/Main.js
@@ -1,11 +1,11 @@
 import HornedBeast from './HornedBeast';
-//import data from './data.json';
+import data from './data.json';
 
 //this is the main component that has 2 hornbeast components with 2 properties each
-function Main() {
+function Main(props) {
     return (
         <div class="d-flex justify-content-center flex-wrap">
-            {props.data.map(Beast=>{
+            {data.map(Beast=>{
                 return(
                 <HornedBeast favorites={0}
                 key={Beast._id}
@@ -16,7 +16,7 @@ function Main() {
                 updateFunction={props.updateFunction}
                 variable={Beast}
                 SelectedBeastFunction={props.SelectedBeastFunction}
-                CurrentBeastInstance={props.currentSelectedBeastVariable}
+                
                 />
 
             )})}
